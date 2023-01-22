b72e4c1295a8dbe92eaa10e8498b312be3e8b17e
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2023-01-22 17:35:57 +0200

    dz 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App extends Component {
+  state = {
+    inputValue: "",
+  };
+
+  handleChange = evt => {
+    this.setState({ inputValue: evt.target.value });
+  };
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <input type="text" value={inputValue} onChange={this.handleChange} />
+    );
+  }
+
+
+
+}
